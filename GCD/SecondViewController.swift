96cb8b0836fab6f6d3b00b23e3c317886eeafd06
Alexey Turulin <repakuku@icloud.com>
2023-10-11 14:52:12 -0400

    get image in main thread

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -8,4 +8,35 @@
 import UIKit
 
 final  class SecondViewController: UIViewController {
+
+	@IBOutlet private weak var imageView: UIImageView!
+
+	private var imageURL: URL?
+	private var image: UIImage? {
+		get {
+			imageView.image
+		}
+		set {
+			imageView.image = newValue
+			imageView.sizeToFit()
+		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		getImage()
+	}
+
+	private func getImage() {
+		imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+
+		guard let imageURL = imageURL else { return }
+
+		do {
+			let imageData = try Data(contentsOf: imageURL)
+			image = UIImage(data: imageData)
+		} catch {
+			print(error)
+		}
+	}
 }
