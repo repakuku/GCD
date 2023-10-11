f2402fbabf4c9109a4d3b2639e3f4ee0962e3641
Alexey Turulin <repakuku@icloud.com>
2023-10-11 15:05:21 -0400

    async getting image

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final  class SecondViewController: UIViewController {
 
 	@IBOutlet private weak var imageView: UIImageView!
+	@IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
 
 	private var imageURL: URL?
 	private var image: UIImage? {
@@ -17,26 +18,36 @@ final  class SecondViewController: UIViewController {
 			imageView.image
 		}
 		set {
+			activityIndicator.stopAnimating()
 			imageView.image = newValue
 			imageView.sizeToFit()
 		}
 	}
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		activityIndicator.hidesWhenStopped = true
 		getImage()
 	}
 
 	private func getImage() {
 		imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
 
+		activityIndicator.startAnimating()
+
 		guard let imageURL = imageURL else { return }
 
-		do {
-			let imageData = try Data(contentsOf: imageURL)
-			image = UIImage(data: imageData)
-		} catch {
-			print(error)
+		let queue = DispatchQueue.global(qos: .utility)
+		queue.async {
+			do {
+				let imageData = try Data(contentsOf: imageURL)
+				DispatchQueue.main.async { [weak self] in
+					guard let self = self else { return }
+					self.image = UIImage(data: imageData)
+				}
+			} catch {
+				print(error)
+			}
 		}
 	}
 }
